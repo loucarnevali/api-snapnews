cb5b342a0410f88718fc1b33336517a98645019f
Louise Carnevali <loucafumee@gmail.com>
2024-04-09 11:26:31 -0400

    debug

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import connectDataBase from './src/database/db.js';
-import cors from 'cors';
+// import cors from 'cors';
 import router from './src/routes/index.js';
 
 const app = express();
 
 connectDataBase();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(router);
 
